09f65de92d5472a616c5c82a4aa337517780b1f4
theEvilReaper <steffenwx@gmail.com>
2022-09-29 20:54:31 +0200

    Add new messages

--- src/main/java/net/theevilreaper/felis/util/Messages.java
+++ src/main/java/net/theevilreaper/felis/util/Messages.java
@@ -2,6 +2,7 @@ package net.theevilreaper.felis.util;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.jetbrains.annotations.NotNull;
 
 public final class Messages {
 
@@ -14,9 +15,19 @@ public final class Messages {
     public static final Component ABORT_TELEPORT_YOURSELF =
             PREFIX.append(Component.text("Can not teleport to yourself", NamedTextColor.RED));
 
+    public static final Component ABORT_FLY_COMMAND =
+            PREFIX.append(Component.text("Command can't be executed in CREATIVE mode", NamedTextColor.RED));
+
     public static final Component NIGHTVISION_ENABLED =
             PREFIX.append(Component.text("Enabled nightvision", NamedTextColor.AQUA));
     public static final Component NIGHTVISION_DISABLED =
             PREFIX.append(Component.text("Disabled nightvision", NamedTextColor.RED));
+
+    public static final Component NO_PERMISSION =
+            buildWithPrefix(Component.text("You can not execute this command!", NamedTextColor.RED));
     private Messages() {}
+
+    public static Component buildWithPrefix(@NotNull Component component) {
+        return PREFIX.append(component);
+    }
 }
