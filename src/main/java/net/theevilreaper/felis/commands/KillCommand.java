ac87231d4dfe2756dce7d63ba75d3a763c1a1c10
theEvilReaper <steffenwx@gmail.com>
2022-10-01 20:31:06 +0200

    Add kill command

---
+++ src/main/java/net/theevilreaper/felis/commands/KillCommand.java
@@ -0,0 +1,49 @@
+package net.theevilreaper.felis.commands;
+
+import net.minestom.server.command.builder.Command;
+import net.minestom.server.command.builder.arguments.ArgumentType;
+import net.minestom.server.command.builder.condition.Conditions;
+import net.minestom.server.entity.Entity;
+import net.minestom.server.entity.Player;
+import net.minestom.server.utils.entity.EntityFinder;
+import net.theevilreaper.felis.util.Messages;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class KillCommand extends Command {
+
+    public KillCommand() {
+        super("kill");
+        this.setCondition(Conditions::playerOnly);
+        var playerArgument = ArgumentType.Entity("player").onlyPlayers(true);
+
+        addSyntax((sender, context) -> {
+            var finder  = context.get(playerArgument);
+            finder.setTargetSelector(EntityFinder.TargetSelector.ALL_PLAYERS);
+            this.execute((Player) sender, finder.find(sender));
+        });
+    }
+
+    private void execute(@NotNull Player player, @NotNull List<Entity> entities) {
+        if (!player.hasPermission("")) {
+            player.sendMessage(Messages.NO_PERMISSION);
+            return;
+        }
+
+        if (entities.isEmpty()) {
+            return;
+        }
+
+        for (int i = 0; i < entities.size(); i++) {
+            var entity = entities.get(i);
+
+            if (entity.getUuid().equals(player.getUuid())) {
+                player.kill();
+                continue;
+            }
+
+            ((Player)entity).kill();
+        }
+    }
+}
