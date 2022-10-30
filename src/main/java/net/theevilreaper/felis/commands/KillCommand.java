22913e895ccc109877c39d267e84b235a635c474
theEvilReaper <steffenwx@gmail.com>
2022-10-30 12:59:03 +0100

    Add some documentation

--- src/main/java/net/theevilreaper/felis/commands/KillCommand.java
+++ src/main/java/net/theevilreaper/felis/commands/KillCommand.java
@@ -11,11 +11,25 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
+/**
+ * The kill command allows a player to kill itself or kill a specific target on the server.<br>
+ * Usage of the command is
+ * <ul>
+ *     <li>/kill</li>
+ *     <li>/kill [target]</li>
+ * </ul>
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 public class KillCommand extends Command {
 
     private static final String KILL_PERMISSION = "felis.command.kill";
     private static final String KILL_OTHER_PERMISSION = "felis.command.kill.other";
 
+    /**
+     * Creates a new instance from the command.
+     */
     public KillCommand() {
         super("kill");
         this.setCondition(Conditions::playerOnly);
@@ -30,6 +44,10 @@ public class KillCommand extends Command {
         }, playerArgument);
     }
 
+    /**
+     * Executes the kill logic for a given player.
+     * @param player the player who is involved in the execution
+     */
     private void executeSelf(@NotNull Player player) {
         if (!player.hasPermission(KILL_PERMISSION)) {
             player.sendMessage(Messages.NO_PERMISSION);
@@ -38,24 +56,25 @@ public class KillCommand extends Command {
         player.kill();
     }
 
+    /**
+     * Executes the command for a given list of targets.
+     * @param player the player who executes the command
+     * @param entities the entities which are defined in an argument
+     */
     private void execute(@NotNull Player player, @NotNull List<Entity> entities) {
         if (!player.hasPermission(KILL_OTHER_PERMISSION)) {
             player.sendMessage(Messages.NO_PERMISSION);
             return;
         }
 
-        if (entities.isEmpty()) {
-            return;
-        }
+        if (entities.isEmpty()) return;
 
         for (int i = 0; i < entities.size(); i++) {
             var entity = entities.get(i);
-
             if (entity.getUuid().equals(player.getUuid())) {
                 executeSelf(player);
                 continue;
             }
-
             ((Player)entity).kill();
         }
     }
