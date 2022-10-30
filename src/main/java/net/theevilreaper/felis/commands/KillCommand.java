a17de96a7b7fc9f47180cc687b597a02c18650b3
theEvilReaper <steffenwx@gmail.com>
2022-10-30 12:41:24 +0100

    Update permission usage

--- src/main/java/net/theevilreaper/felis/commands/KillCommand.java
+++ src/main/java/net/theevilreaper/felis/commands/KillCommand.java
@@ -14,21 +14,32 @@ import java.util.List;
 public class KillCommand extends Command {
 
     private static final String KILL_PERMISSION = "felis.command.kill";
+    private static final String KILL_OTHER_PERMISSION = "felis.command.kill.other";
 
     public KillCommand() {
         super("kill");
         this.setCondition(Conditions::playerOnly);
         var playerArgument = ArgumentType.Entity("player").onlyPlayers(true);
 
+        addSyntax((sender, context) -> this.executeSelf((Player)sender));
+
         addSyntax((sender, context) -> {
             var finder  = context.get(playerArgument);
             finder.setTargetSelector(EntityFinder.TargetSelector.ALL_PLAYERS);
             this.execute((Player) sender, finder.find(sender));
-        });
+        }, playerArgument);
+    }
+
+    private void executeSelf(@NotNull Player player) {
+        if (player.hasPermission(KILL_PERMISSION)) {
+            player.sendMessage(Messages.NO_PERMISSION);
+            return;
+        }
+        player.kill();
     }
 
     private void execute(@NotNull Player player, @NotNull List<Entity> entities) {
-        if (!player.hasPermission(KILL_PERMISSION)) {
+        if (!player.hasPermission(KILL_OTHER_PERMISSION)) {
             player.sendMessage(Messages.NO_PERMISSION);
             return;
         }
@@ -41,7 +52,7 @@ public class KillCommand extends Command {
             var entity = entities.get(i);
 
             if (entity.getUuid().equals(player.getUuid())) {
-                player.kill();
+                executeSelf(player);
                 continue;
             }
 
